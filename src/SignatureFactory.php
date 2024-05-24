93243f11b284d00b4dfdf9ac20d1c607b38b9709
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-05-24 16:34:01 +0800

    updated

--- src/SignatureFactory.php
+++ src/SignatureFactory.php
@@ -1,9 +1,9 @@
 <?php 
 
-namespace Beycan\SignatureFactory;
+namespace MultipleChain\SignatureFactory;
 
-use \Beycan\SignatureFactory\Dependencies\Keccak\Keccak256;
-use \Beycan\SignatureFactory\Dependencies\Elliptic\EC;
+use \MultipleChain\SignatureFactory\Dependencies\Elliptic\EC;
+use \MultipleChain\SignatureFactory\Dependencies\Keccak\Keccak256;
 
 class SignatureFactory
 {
@@ -18,6 +18,7 @@ class SignatureFactory
     }
 
     /**
+     * Extract signed address from message and signature
      * @param string $message
      * @param string $signature
      * @return string|null
@@ -25,63 +26,23 @@ class SignatureFactory
     public static function extractAddress(string $message, string $signature) : ?string
     {
         if (extension_loaded('mbstring') && extension_loaded('gmp') || extension_loaded('bcmath')) {
-            return self::extractAddressWithPhp($message, $signature);
+            $message = "\31Ethereum Signed Message:\n" . strlen($message) . $message;
+    
+            $ec = new EC('secp256k1');
+            $messageHash = Keccak256::hash($message, 256);
+    
+            $sign = [
+                'r' => substr($signature, 2, 64),
+                's' => substr($signature, 66, 64),
+            ];
+    
+            $recId = ord(hex2bin(substr($signature, 130, 2))) - 27;
+    
+            $address = $ec->recoverPubKey($messageHash, $sign, $recId);
+    
+            return '0x' . substr(Keccak256::hash(substr(hex2bin($address->encode('hex')), 1), 256), 24);
         } else {
-            return self::extractAddressWithApi($message, $signature);
+            throw new \Exception('mbstring and gmp or bcmath extensions are required');
         }
     }
-
-    /**
-     * @param string $message
-     * @param string $signature
-     * @return string
-     */
-	private static function extractAddressWithPhp(string $message, string $signature) : string
-    {
-        $message = "\31Ethereum Signed Message:\n" . strlen($message) . $message;
-
-        $messageHash = Keccak256::hash($message, 256);
-        $ec = new EC('secp256k1');
-
-        $sign = [
-            'r' => substr($signature, 2, 64),
-            's' => substr($signature, 66, 64),
-        ];
-
-        $recid = ord(hex2bin(substr($signature, 130, 2))) - 27;
-
-        $pubKey = $ec->recoverPubKey($messageHash, $sign, $recid);
-
-        return '0x' . substr(Keccak256::hash(substr(hex2bin($pubKey->encode('hex')), 1), 256), 24);
-    }
-
-    /**
-     * @param string $message
-     * @param string $signature
-     * @return string|null
-     */
-	private static function extractAddressWithApi(string $message, string $signature) : ?string
-    {
-        $data = [
-            'message'   => $message,
-            'signature' => $signature,
-        ];
-		
-		$curl = curl_init('https://extract-address.herokuapp.com/');
-		curl_setopt_array($curl, [
-			CURLOPT_POST => true,
-			CURLOPT_POSTFIELDS => $data,
-			CURLOPT_RETURNTRANSFER => true
-		]);
-		
-		$result = curl_exec($curl);
-		curl_close($curl);
-        
-        if (!empty($result) ) {
-            return json_decode($result)->data->address;
-        } else {
-            return null;
-        }
-    }
-
 }
